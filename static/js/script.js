b58250237af0af8b15dd226f81a856e8a719356b
JamieT966 <jamietarpey1@gmail.com>
2022-09-28 14:31:54 +0000

    used javascript to make search input appear

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
 const searchBtn = document.getElementById('search-icon-btn')
 const closeBtn = document.getElementById('search-close-btn')
-const searchOverlay = document.getElementById('search-overlay')
+const searchDiv = document.getElementById('search-overlay')
 
-searchBtn.addEventListener('click', () => {
-    if(searchOverlay.className == 'd-none' ) {
-        searchOverlay.className == ''
-    }
-});
+function unhideSearch() {
+    searchDiv.classList.remove('d-none')
+}
+
+function hideSearchy() {
+    searchDiv.classList.add('d-none')
+}
