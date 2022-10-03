49b9303eb7236afd8f30baa4be67ce7b89fe5888
JamieT966 <jamietarpey1@gmail.com>
2022-10-03 15:21:54 +0000

    full redesign of all products page, I wasn't happy with it

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
     """
-    Displays all products page, includes sorting and search queries
+    Displays a single product page
     """
 
     products = Product.objects.all()
@@ -15,3 +15,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    Displays all products page, includes sorting and search queries
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': product,
+    }
+
+    return render(request, 'products/products_detail.html', context)
