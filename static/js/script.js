1ee31272d8b3719849ec37937acf677b1fe02ddf
JamieT966 <jamietarpey1@gmail.com>
2022-10-05 15:02:23 +0000

    added javascript to make sort by work

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// Allows opening and closing of search button 
 const searchBtn = document.getElementById('search-icon-btn')
 const closeBtn = document.getElementById('search-close-btn')
 const searchDiv = document.getElementById('search-overlay')
@@ -9,3 +10,25 @@ function unhideSearch() {
 function hideSearchy() {
     searchDiv.classList.add('d-none')
 }
+
+// Allows user to select sorting criteria, taken from Boutique Ado
+$('#sort-selector').change(function() {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+})
