31a0e7c030cb17aebb0922988474a9cd18263dcd
JamieT966 <jamietarpey1@gmail.com>
2022-10-13 13:32:22 +0000

    fixed bug that was causing page not to refresh

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,13 +1,13 @@
 /*
     Core logic/payment flow for this comes from here:
-    https://stripe.com/docs/development/quickstart?lang=python
+    https://stripe.com/docs/payments/accept-a-payment
 
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecrert = $('#id_client_secret').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -28,24 +28,23 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle realtine validation and error on card element
-
-card.addEventListener('change', function(event) {
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
-        <span role="alert">
-            <i class="fas fa-times"></i>
-        </span>
-        <span>${event.error.message}</span>`
-
+            <span role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
 });
 
-// From Stripe, code to handle form submit
+// Basic function from Stripe. Code to handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
@@ -60,10 +59,10 @@ form.addEventListener('submit', function(ev) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
-            <span role="alert">
+                <span role="alert">
                 <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>`;
+                </span>
+                <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
