43d50039853faceacc46e8bbcfa97254d13f4a80
JamieT966 <jamietarpey1@gmail.com>
2022-10-16 20:23:36 +0000

    Completed confirmation emails

--- chemstore/settings.py
+++ chemstore/settings.py
@@ -179,3 +179,6 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_KEY', '')
+
+# Confirmation emails
+DEFAULT_FROM_EMAIL = sales@chemstore.ie
