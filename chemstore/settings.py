a8218c3decbf77ead5b628b245890b60e7e48d93
JamieT966 <jamietarpey1@gmail.com>
2022-10-17 20:54:24 +0000

    new secret key

--- chemstore/settings.py
+++ chemstore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p+zb985nwk2(w2h#4vo#=gm=6gyj2td!($^kwlt@c9yvdd7qi5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chem-eshop.herokuapp.com', 'localhost']
 
