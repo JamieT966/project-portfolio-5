1ee8b17291e2cd48bffa39e6474affc5f49ec2d6
JamieT966 <jamietarpey1@gmail.com>
2022-10-21 10:43:05 +0000

    created draft order app and model

---
+++ draft_order/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class DraftOrder(models.Model):
+
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    customer_phone = models.CharField(max_length=10)
+    message = models.TextField(max_length=600) #If applicable
+    created_at = models.DateTimeField(auto_now_add=True)
