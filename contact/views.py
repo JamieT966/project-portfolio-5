bc087bae370dd2324fe060cf73947aeb7fcb727f
JamieT966 <jamietarpey1@gmail.com>
2022-10-21 16:01:41 +0000

    set up email to be sent to staff member when contact form is used

--- contact/views.py
+++ contact/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render
 from django.contrib import messages
 from .models import ContactForm
 from .forms import DisplayContactForm
+from django.core.mail import send_mail
 
 
 def contact(request):
     """
     Displays contact form
+    When contact form is submitted send user success message
+    Sends email to staff about message
     """
     form = DisplayContactForm(request.POST or None)
     if form.is_valid():
         form.save()
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message = form.cleaned_data['message']
+        product = form.cleaned_data['product']
+
         form = DisplayContactForm()
         messages.success(request, 'Message received, a member of our team will get back to you shortly.')
+
+        # Send an email to Chemstore Admin
+        send_mail(
+            f'New Message from {name} relating to {product}',
+            message,
+            email,
+            ['jamie@chem-eshop.ie']
+            )
 
     template = 'contact/contact.html'
     context = {
