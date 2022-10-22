ffc5fe12f4967e7d7042d81564742787127a434b
JamieT966 <jamietarpey1@gmail.com>
2022-10-22 17:01:00 +0000

    view to display newsletter sign up form and logic addded

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import NewsletterSubscriber
+from .forms import NewsletterSubscriberForm
+from django.contrib import messages
 
-# Create your views here.
+
+def display_newsletter_fom(request):
+    form = NewsletterSubscriberForm(request.POST or None)
+
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        instance = form.save(commit=False)
+
+        if NewsletterSubscriber.objects.filter(email=instance.email).exists():
+            messages.error(request, 'Sorry, this email already exists in our system.')
+        else:
+            instance.save()
+
+        context = {
+            'form': form,
+        }
+
+        template = 'index.html'
+
+        return render(request, template, context)
