8e5b8cf4b87276e9bdaf36c851395a03319ec524
JamieT966 <jamietarpey1@gmail.com>
2022-10-28 18:27:43 +0000

    changed rating to integerfield with choices

--- products/models.py
+++ products/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from profiles.models import UserProfile
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Avg
+
+RATING_OPTIONS = (
+    (0,'0'),
+    (0.5,'0.5'),
+    (1,'1'),
+    (1.5,'1.5'),
+    (2,'2'),
+    (2.5,'2.5'),
+    (3,'3'),
+    (3.5,'3.5'),
+    (4,'4'),
+    (4.5,'4.5'),
+    (5,'5'),
+    )
 
 
 class Category(models.Model):
@@ -26,20 +41,26 @@ class Product(models.Model):
     description = RichTextField(blank=True, null=True)
     has_sizes = models.BooleanField(null=True, blank=True, default=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    # Advised by my mentor Brian Macharia on how to have this work
+    def rating(self):
+        reviews = Review.objects.filter(product=self, status='True').aggregate(avarage=Avg('rating'))
+        avg=0
+        if reviews["avarage"] is not None:
+            avg=float(reviews["avarage"])
+        return avg
+
 
 class Review(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE, related_name='reviews')
     title = models.CharField(max_length=80, blank=True)
     review = models.TextField(max_length=1000, blank=True)
-    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+    rating = models.IntegerField(choices=RATING_OPTIONS, default=3)
     status = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
